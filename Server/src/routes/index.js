36c4d1e96e9afec20c582cf25a3d97ef8ff1e0bb
Matias <mati.dehoyosmdp@gmail.com>
2024-12-17 18:25:19 +0000

    corrections

--- Server/src/routes/index.js
+++ Server/src/routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const getCharById = require("../controllers/getCharById");
-const getCharByName = require("../controllers/getCharByName");
-const getChars = require("../controllers/getChars");
+const getCharById = require("../controllers/getCharById.js");
+const getCharByName = require("../controllers/getCharByName.js");
+const getChars = require("../controllers/getChars.js");
 
 router.get("/characters", getChars);
 router.get("/character/:id", getCharById);
